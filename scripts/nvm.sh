acec0703fef904179ce79fbe6ec7b93826c32197
Gabriel Nascimento <135087316+gabrielgnsilva@users.noreply.github.co>
2023-06-04 17:59:48 -0300

    Added nvm fallback install folder

--- scripts/nvm.sh
+++ scripts/nvm.sh
@@ -4,7 +4,7 @@
 
 set +o nounset
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-export NVM_DIR="${HOME}/.config/nvm"
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "${NVM_DIR}/nvm.sh" ] && \. "${NVM_DIR}/nvm.sh"                    # This loads nvm
 [ -s "${NVM_DIR}/bash_completion" ] && \. "${NVM_DIR}/bash_completion"  # This loads nvm bash_completion
 nvm install --lts
