7cbe15ec3f291ee37d4bc7f5ab8c51cf75038da5
Gabriel Nascimento <135087316+gabrielgnsilva@users.noreply.github.co>
2023-06-04 18:00:38 -0300

    Added XDG_DATA_HOME and fixed fonts not being applied.

--- scripts/fonts.sh
+++ scripts/fonts.sh
@@ -2,39 +2,50 @@
 
 #! THIS FILE MUST BE SOURCED FROM "install" !
 
+# Fonts directory
+fonts_dir="$([ -z "${XDG_DATA_HOME-}" ] && printf %s "${HOME}/.local/share/fonts" || printf %s "${XDG_DATA_HOME}/fonts")"
+mkdir --parents --verbose "${fonts_dir}"
+
 # Icons (Material Design Fonts)
+if [ -d "${fonts_dir}"/MaterialDesign-Font ]; then
+    rm --force --recursive --verbose "${fonts_dir}"/MaterialDesign-Font
+fi
+
 git clone https://github.com/Templarian/MaterialDesign-Font.git "${script_tempdir}"/MaterialDesign-Font/
 
+cp --recursive --verbose "${script_tempdir}"/MaterialDesign-Font/ "${fonts_dir}"
+
 # Nerd Fonts (Hack)
+if [ -d "${fonts_dir}"/Hack ]; then
+    rm --force --recursive --verbose "${fonts_dir}"/Hack
+fi
+
 version=$(curl -s https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest \
-| grep "tag_name"                                                                   \
-| awk '{ print $2 }'                                                                \
-| sed 's/,$//'                                                                      \
-| sed 's/"//g' )                                                                    \
-; curl --location https://github.com/ryanoasis/nerd-fonts/releases/download/"${version}"/Hack.zip --output "${script_tempdir}"/Hack.zip
+    | grep "tag_name"                                                               \
+    | awk '{ print $2 }'                                                            \
+    | sed 's/,$//'                                                                  \
+    | sed 's/"//g' )
+
+curl --location https://github.com/ryanoasis/nerd-fonts/releases/download/"${version}"/Hack.zip --output "${script_tempdir}"/Hack.zip
+
+mkdir --verbose --parents "${fonts_dir}"/Hack
+unzip -o "${script_tempdir}"/'Hack.zip' -d "${fonts_dir}"/Hack/
 
 # Nerd Fonts (FiraCode)
+if [ -d "${fonts_dir}"/FiraCode ]; then
+    rm --force --recursive --verbose "${fonts_dir}"/FiraCode
+fi
+
 version=$(curl -s https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest \
-| grep "tag_name"                                                                   \
-| awk '{ print $2 }'                                                                \
-| sed 's/,$//'                                                                      \
-| sed 's/"//g' )                                                                    \
-; curl --location https://github.com/ryanoasis/nerd-fonts/releases/download/"${version}"/FiraCode.zip --output "${script_tempdir}"/FiraCode.zip
+    | grep "tag_name"                                                               \
+    | awk '{ print $2 }'                                                            \
+    | sed 's/,$//'                                                                  \
+    | sed 's/"//g' )
 
-# Install Fonts
+curl --location https://github.com/ryanoasis/nerd-fonts/releases/download/"${version}"/FiraCode.zip --output "${script_tempdir}"/FiraCode.zip
 
-mkdir --parents --verbose "${HOME}"/.local/share/fonts
-
-if [ -d "${HOME}"/.local/share/fonts/MaterialDesign-Font ]; then rm --force --recursive --verbose "${HOME}"/.local/share/fonts/MaterialDesign-Font; fi
-cp --recursive "${script_tempdir}"/MaterialDesign-Font/ "${HOME}"/.local/share/fonts/
-
-if [ -d "${HOME}"/.local/share/fonts/Hack ]; then rm --force --recursive --verbose "${HOME}"/.local/share/fonts/Hack; fi
-mkdir --verbose --parents "${HOME}"/.local/share/fonts/Hack
-unzip -o "${script_tempdir}"/'Hack.zip' -d "${HOME}"/.local/share/fonts/Hack/
-
-if [ -d "${HOME}"/.local/share/fonts/FiraCode ]; then rm --force --recursive --verbose "${HOME}"/.local/share/fonts/FiraCode; fi
-mkdir --verbose --parents "${HOME}"/.local/share/fonts/FiraCode
-unzip -o "${script_tempdir}"/'FiraCode.zip' -d "${HOME}"/.local/share/fonts/FiraCode/
+mkdir --verbose --parents "${fonts_dir}"/FiraCode
+unzip -o "${script_tempdir}"/'FiraCode.zip' -d "${fonts_dir}"/FiraCode/
 
 # Refresh Fonts Cache
 fc-cache --really-force
