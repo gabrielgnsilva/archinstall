bbe459025f3a6dd4fd83bbe426bf86e58dc9085c
Gabriel Nascimento <135087316+gabrielgnsilva@users.noreply.github.com>
2025-05-18 17:03:37 -0300

    Add and update package list and configurations

--- scripts/fonts.sh
+++ scripts/fonts.sh
@@ -1,51 +1,45 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 #! THIS FILE MUST BE SOURCED FROM "install" !
 
 # Fonts directory
 fontsDir="$([ -z "${XDG_DATA_HOME-}" ] && printf %s "${HOME}/.local/share/fonts" || printf %s "${XDG_DATA_HOME}/fonts")"
-mkdir --parents --verbose "${fontsDir}"
+mkdir --parents --verbose "${fontsDir:?}"
 
-# Icons (Material Design Fonts)
+# region: Material Design Fonts
 if [ -d "${fontsDir}"/MaterialDesign-Font ]; then
     rm --force --recursive --verbose "${fontsDir}"/MaterialDesign-Font
 fi
-
-git clone https://github.com/Templarian/MaterialDesign-Font.git "${scriptTempDir}"/MaterialDesign-Font/
-
+git clone https://github.com/Templarian/MaterialDesign-Font.git "${scriptTempDir:?}"/MaterialDesign-Font/
 cp --recursive --verbose "${scriptTempDir}"/MaterialDesign-Font/ "${fontsDir}"
+# regionend
 
-# Nerd Fonts (Hack)
-if [ -d "${fontsDir}"/Hack ]; then
-    rm --force --recursive --verbose "${fontsDir}"/Hack
-fi
+function download_nerd_font() {
+    local font=${1}
+    local version
+    if [ -d "${fontsDir:?}"/"${font:?}" ]; then
+        rm --force --recursive --verbose "${fontsDir:?}"/"${font:?}"
+    fi
+    version=$(curl -s https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest \
+        | grep "tag_name" \
+        | awk '{ print $2 }' \
+        | sed 's/,$//' \
+        | sed 's/"//g')
 
-version=$(curl -s https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest \
-    | grep "tag_name"                                                               \
-    | awk '{ print $2 }'                                                            \
-    | sed 's/,$//'                                                                  \
-    | sed 's/"//g' )
+    printf "%b\n" "Installing ${font} - ${version}" >&3
 
-curl --location https://github.com/ryanoasis/nerd-fonts/releases/download/"${version}"/Hack.zip --output "${scriptTempDir}"/Hack.zip
+    curl --location https://github.com/ryanoasis/nerd-fonts/releases/download/"${version}"/"${font:?}".zip --output "${scriptTempDir:?}"/"${font:?}".zip
 
-mkdir --verbose --parents "${fontsDir}"/Hack
-unzip -o "${scriptTempDir}"/'Hack.zip' -d "${fontsDir}"/Hack/
+    mkdir --verbose --parents "${fontsDir:?}"/"${font:?}"
+    unzip -o "${scriptTempDir:?}"/"${font:?}".zip -d "${fontsDir:?}"/"${font:?}"/
+}
 
-# Nerd Fonts (FiraCode)
-if [ -d "${fontsDir}"/FiraCode ]; then
-    rm --force --recursive --verbose "${fontsDir}"/FiraCode
-fi
-
-version=$(curl -s https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest \
-    | grep "tag_name"                                                               \
-    | awk '{ print $2 }'                                                            \
-    | sed 's/,$//'                                                                  \
-    | sed 's/"//g' )
-
-curl --location https://github.com/ryanoasis/nerd-fonts/releases/download/"${version}"/FiraCode.zip --output "${scriptTempDir}"/FiraCode.zip
-
-mkdir --verbose --parents "${fontsDir}"/FiraCode
-unzip -o "${scriptTempDir}"/'FiraCode.zip' -d "${fontsDir}"/FiraCode/
+download_nerd_font Hack
+download_nerd_font FiraCode
+download_nerd_font CascadiaCode
 
 # Refresh Fonts Cache
 fc-cache --really-force
+
+unset -v fontsDir
+unset f download_nerd_font
---
+++ scripts/tpm.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+mkdir --parents ~/.config/tmux/plugins
+git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
